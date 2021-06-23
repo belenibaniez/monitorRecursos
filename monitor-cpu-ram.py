8aa5352ce59eab16bc162e8bd1e5edf8cc54d27d
Belen Ibañez <belenibaniez@gmail.com>
2021-06-23 11:45:33 -0300

    prueba

--- monitor-cpu-ram.py
+++ monitor-cpu-ram.py
@@ -1,33 +1,14 @@
 import psutil
 from datetime import datetime
-
-def generarInforme():
-    cpuCount = psutil.cpu_count () #cpu logicos
-    print(cpuCount)
-    
+import os
 
 
 def run():
     while True:
-        print(psutil.cpu_percent(interval=1, percpu=True))
+        comando1= "top -bn1 | grep load "
+        print(os.popen(comando1)
 
-        
-
-
-
-#cpulogical = psutil.cpu_count (lógico = "Falso") # núcleo físico de la CPUç
-#print ('núcleo físico de la CPU:', cpulogical)
-
-
-cpuTimes = psutil.cpu_times()
-#print ('usuario de CPU / sistema / tiempo de inactividad:', cpuTimes)
-
-#print(psutil.cpu_percent())
-#print(psutil.virtual_memory())
-
-
-#print(dict(psutil.virtual_memory()._asdict()))
-
+    
 
 if __name__=="__main__":
     run()
